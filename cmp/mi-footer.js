18dba9c0060c38583b2f43f74517f09910618c0a
DanyHammettt <76023871+DanyHammettt@users.noreply.github.com>
2021-01-23 15:03:16 -0600

    Delete cmp directory

--- cmp/mi-footer.js
+++
@@ -1,7 +0,0 @@
-class MiFooter extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = /* html */
-            `Copyright &copy; 2021 Daniel Franco Tinajero.`;
-    }
-}
-customElements.define("mi-footer", MiFooter);
