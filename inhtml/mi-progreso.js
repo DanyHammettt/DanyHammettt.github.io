e2002cdd87ff311cb13341553dbf0167bdfce8c4
DanyHammettt <76023871+DanyHammettt@users.noreply.github.com>
2021-01-23 15:04:24 -0600

    Delete inhtml directory

--- inhtml/mi-progreso.js
+++
@@ -1,7 +0,0 @@
-class MiProgeso extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = /* html */
-      `<progress max="100"><Iniciando…</progress>`;
-  }
-}
-customElements.define("mi-progreso", MiProgeso);
