65cf990fb21c4cdfd3cbb9b4e129e11f11ffd719
DanyHammettt <76023871+DanyHammettt@users.noreply.github.com>
2021-01-23 15:20:21 -0600

    Delete inhtml directory

--- inhtml/Progreso.js
+++
@@ -1,7 +0,0 @@
-class Progresobar extends HTMLElement{
-	connectedCallback(){
-		this.innerHTML = /* html */
-		`<progress max="1000"><Iniciando…</progress>`;
-	}
-}
-customElements.define("barra-progreso",Progresobar);
--- inhtml/footer.js
+++
@@ -1,7 +0,0 @@
-class Footer extends HTMLElement{
-	connectedCallback(){	
-		this.innerHTML = /* html */
-			'Original de &copy; 2021 Daniel Franco Tinajero';
-	}
-}
-customElements.define("pie-footer",Footer);
