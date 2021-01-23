d3470d2bfba388df37380ab0f5605cca9113bcd1
DanyHammettt <76023871+DanyHammettt@users.noreply.github.com>
2021-01-23 15:40:45 -0600

    Add files via upload

--- cmp/mi-progreso.js
+++ cmp/mi-progreso.js
@@ -1,7 +1,7 @@
 class MiProgeso extends HTMLElement {
   connectedCallback() {
     this.innerHTML = /* html */
-      `<progress max="100"><Iniciando…</progress>`;
+      `<progress max="100">Cargando…</progress>`;
   }
 }
 customElements.define("mi-progreso", MiProgeso);
